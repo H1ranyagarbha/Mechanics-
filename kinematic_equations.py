e062daef90f915304514af1c936bec800e730b17
Hiranyagarbha <88590577+H1ranyagarbha@users.noreply.github.com>
2023-04-21 11:49:45 +0530

    Update kinematic_equations.py

--- kinematic_equations.py
+++ kinematic_equations.py
@@ -4,28 +4,48 @@ print("Hello, what values do you know? For the value you need to find, type x in
 print()
 
 u = input("What is the initial velocity:")
+print(u)
 v = input ("What is the final velocity:")
+print(v)
 a = input("What is the acceleration:")
+print(a)
 t = input ("What is the time taken:")
+print(t)
 s = input ("What is the displacement:")
+print(s)
 print()
+
 if (u = x and s = x):
 
-    choice_1 = 
-    print ("So the initial velocity is unknown")
-    print("So the final velocity is " + v + "m/s")
-    print("So the acceleration is " + a + "m/s^2")
-    print("So the time  is " + t + "s")
-    print("So the displacement is " + s + "m")
-    print()
-    print("calculating intial velocity....")
-    intial_velocity = v - a*t
-    print()
-    print ("The intial velocity is " + intial_velocity)
+    choice_1 = input("Which value do you want to find?: ")
+    print(choice_1)
+    if (choice_1 = u):
+        print("calculating intial velocity....")
+        intial_velocity_1 = v - a*t
+        print()
+        print ("The intial velocity is " + intial_velocity_1)
+    elif (choice_1 = s):
+        print ("calculating displacement.....")
+        displacement_1 = intial_velocity_1*t + (1/2)*a*(t)^2
+        print()
+        print("The displacement is:" + displacement_1)
+    else:
+        print ("Please rerun the code, you've selected a wrong quantity!")
 
-elif u = x and t = x 
+     
 
-elif (v = x):
+elif (u = x and t = x): 
+    choice_2 = input("Which value do you want to find: ")
+    print(choice_2)
+    if (choice_2 = u):
+        print("calculating intial velocity....")
+        intial_velocity_2 = v - a*t
+        print()
+        print ("The intial velocity is " + intial_velocity_2)
+    else:
+        print ("Please rerun the code, you've selected a wrong quantity!")
+
+elif (v = x and t=x):
     print ("So the final velocity is unknown")
     print("So the initial velocity is " + u + "m/s")
     print("So the acceleration is " + a + "m/s^2")
