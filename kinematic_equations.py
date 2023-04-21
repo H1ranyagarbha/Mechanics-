84934f93dad662155bdc10db68c1fa2131d16176
Hiranyagarbha <88590577+H1ranyagarbha@users.noreply.github.com>
2023-04-21 07:51:15 +0530

    Create kinematic_equations.py
    temp

---
+++ kinematic_equations.py
@@ -0,0 +1,40 @@
+import numpy as n 
+
+print("Hello, what values do you know? For the value you need to find, type x in it's place.")
+print()
+
+u = input("What is the initial velocity:")
+v = input ("What is the final velocity:")
+a = input("What is the acceleration:")
+t = input ("What is the time taken:")
+s = input ("What is the displacement:")
+print()
+if (u = x and s = x):
+
+    choice_1 = 
+    print ("So the initial velocity is unknown")
+    print("So the final velocity is " + v + "m/s")
+    print("So the acceleration is " + a + "m/s^2")
+    print("So the time  is " + t + "s")
+    print("So the displacement is " + s + "m")
+    print()
+    print("calculating intial velocity....")
+    intial_velocity = v - a*t
+    print()
+    print ("The intial velocity is " + intial_velocity)
+
+elif u = x and t = x 
+
+elif (v = x):
+    print ("So the final velocity is unknown")
+    print("So the initial velocity is " + u + "m/s")
+    print("So the acceleration is " + a + "m/s^2")
+    print("So the time  is " + t + "s")
+    print("So the displacement is " + s + "m")
+    print()
+    print("calculating final velocity....")
+    final_velocity = u + a*t
+    print()
+    print ("The final velocity is " + final_velocity)
+
+elif a = x 
